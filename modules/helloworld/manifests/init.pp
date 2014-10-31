8d2c8debf5b08a555ecdf080fe3b16e9d07a8a25
nikolaia <nikolai.andersen@bekk.no>
2014-10-31 12:46:43 +0100

    Defined a powershell file in the helloworld module that is copied to the node and then executed

--- modules/helloworld/manifests/init.pp
+++ modules/helloworld/manifests/init.pp
@@ -2,4 +2,15 @@ class helloworld {
 	notify { 
 		'Hello World!': 
 	}
+
+	file {'C:\helloworld.ps1':
+		source 	=> "puppet:///modules/helloworld/helloworld.ps1"
+	}
+
+	exec { 'runPowershell':
+	  command   => '.\helloworld.ps1',
+	  cwd				=> 'C:\\',
+	  provider  => powershell,
+	  logoutput => true
+	}
 }
